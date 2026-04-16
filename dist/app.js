302bb27e9db071dfe72071eb5b96eec773d7ca20
Emanuel Eirich <shanks-@gmx.de>
2026-04-16 19:57:42 +0200

    .

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,22 @@
-console.log("object123");
+console.log("I love Nadine");
+let cursorDot = document.querySelector(".cursor-dot");
+document.addEventListener("mouseover", (e) => {
+    if (cursorDot)
+        return;
+    cursorDot = document.createElement("div");
+    cursorDot.classList.add("cursor-dot");
+    document.body.appendChild(cursorDot);
+});
+document.addEventListener("mousemove", (e) => {
+    if (!cursorDot)
+        return;
+    cursorDot.style.left = `${e.clientX}px`;
+    cursorDot.style.top = `${e.clientY}px`;
+});
+document.addEventListener("mouseout", (e) => {
+    if (!cursorDot)
+        return;
+    cursorDot.remove();
+    cursorDot = null;
+});
 export {};
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,23 @@
 
 console.log("I love Nadine")
+
+let cursorDot = document.querySelector(".cursor-dot") as HTMLElement | null;
+
+document.addEventListener("mouseover", (e) => {
+   if (cursorDot) return
+   cursorDot = document.createElement("div");
+   cursorDot.classList.add("cursor-dot");
+   document.body.appendChild(cursorDot);
+});
+
+document.addEventListener("mousemove", (e) => {
+   if (!cursorDot) return
+   cursorDot.style.left = `${e.clientX}px`;
+   cursorDot.style.top = `${e.clientY}px`;
+})
+
+document.addEventListener("mouseout", (e) => {
+   if (!cursorDot) return
+   cursorDot.remove();
+   cursorDot = null;
+})
