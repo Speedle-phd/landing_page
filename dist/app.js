7e485e3de444ea01d4c85f8c948948042c880a2c
root <root@my-vps>
2026-05-14 19:22:56 +0000

    .

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,4 @@
-console.log("I love Nadine");
+console.log("I love Nadine so much!!!!!!!");
 let cursorDot = document.querySelector(".cursor-dot");
 document.addEventListener("mouseover", (e) => {
     if (cursorDot)
